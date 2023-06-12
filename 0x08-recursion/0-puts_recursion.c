878a629d2b5c8881816b0bd420a1dd25d813cd18
zeethebeast <chimasamuel6@gmail.com>
2023-06-12 09:02:45 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 		if(*s == '\0')/*base case*/
 	{
 	_putchar(*s);/*prints current character*/
-	_puts_recursion(s+1);/*call function with next character in the string*/
+	_puts_recursion(s + 1);/*call function with next character in the string*/
 	}
 	else 
 		_putchar('\n');
