fa3bacec439660bc84d2d3ef5d737eb345c49d43
Zeethebeast <chimasamuel6@gmail.com>
2023-09-13 06:26:15 -0700

    repo

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * main - displays the file name
+ *
+ * Return: always 0.
+ */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	
-	return 0;
+
+	return (0);
 }
