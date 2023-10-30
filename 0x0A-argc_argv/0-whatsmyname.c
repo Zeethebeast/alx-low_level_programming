2c5e529b2b95f6bbdb9287a434e776b0c6462e36
Zeethebeast <chimasamuel6@gmail.com>
2023-10-30 09:45:55 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints a new name without compiling
+ * @argc: counts the number of output
+ * @argv: holds string values
+ * Return: returns 0
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
