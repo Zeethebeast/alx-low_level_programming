328f0eab93d786a663632ac371a2c4e3145ede63
Zeethebeast <chimasamuel6@gmail.com>
2023-11-21 17:56:34 -0800

    more singly lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - returns the number of elements
+ * @h: header pointer to the next node
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *newnode = h;
+
+	while (newnode != 0)
+	{
+
+	count++;
+	newnode = newnode->next;
+	}
+	return (count);
+}
