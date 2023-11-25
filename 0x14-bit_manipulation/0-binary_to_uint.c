1fcda4532e0eb66bcf0e190f28e2ee0582543cad
Zeethebeast <chimasamuel6@gmail.com>
2023-11-25 07:28:23 -0800

    bit manipulaton

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal = 0;
 	int i = 0;
-	
+
 	if (b == NULL)
 	return (0);
 
-	for (;b[i] != '\0'; i++)
+	for (; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
