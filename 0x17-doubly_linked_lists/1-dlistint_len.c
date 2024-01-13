a10d5a1d2188e71099fd277ddf00e735e51da149
Zeethebeast <chimasamuel6@gmail.com>
2024-01-12 22:58:18 -0800

    doubly inked list tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -15,7 +15,7 @@ size_t dlistint_len(const dlistint_t *h)
         if (h == NULL)
                 return (count);
 
-        while (h-> prev != NULL)
+        while (h-> rev != NULL)
                 h = h->prev;
 
         while (h != NULL)
